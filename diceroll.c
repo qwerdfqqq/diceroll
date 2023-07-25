baec7e9043f1ca5d0d09053bd4df73ea59d5fe42
qwerdfqqq <99341215+qwerdfqqq@users.noreply.github.com>
2023-07-25 19:31:32 +0800

    Add files via upload

---
+++ diceroll.c
@@ -0,0 +1,38 @@
+#include "diceroll.c"
+#include <stdio.h>
+#include <stdlib.h>
+
+int roll_count = 0;
+
+static int rollem(int sides)
+{
+    int roll;
+
+    roll = rand() % sides + 1;
+    ++roll_count;
+
+    return roll;
+}
+
+int roll_n_dice(int dice, int sides)
+{
+    int d;
+    int total = 0;
+    if (sides < 2)
+    {
+        printf("Need at least 2 sides.\n")
+        return -2;
+    }
+    if (dice < 1)
+    {
+        printf("Need at least 1 die\n")
+        return -1;
+    }
+
+    for (int i = 0; i < dice; i++)
+    {
+        total += rollem(sides);
+    }
+    
+    return total;
+}
